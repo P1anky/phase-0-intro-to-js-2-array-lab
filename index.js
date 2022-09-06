6a79253e0d5b1be4bff521dbac8cded78e11f193
Rob Sanders Hewett <robsh15@gmail.com>
2022-09-06 12:59:47 +0100

    commit tree ee3308594d11851e715362060ebecd42882a188e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(x){
+    cats.push(x)
+}
+function destructivelyPrependCat(x){
+    cats.unshift(x)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop(1)
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift(1)
+}
+function appendCat(x){
+    return[...cats,x]
+}
+function prependCat(x){
+    return[x,...cats]
+}
+function removeLastCat(){
+    return(cats.slice(0,cats.length-1))
+}
+function removeFirstCat(){
+    return(cats.slice(1))
+}
